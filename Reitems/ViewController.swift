cafe9d7bf9688c767f4c6fbe85cc02fe68f4eeb7
Dennis Anthony <dennisanthonyblabla@gmail.com>
2022-04-25 16:30:26 +0700

    Initial Commit

---
+++ Reitems/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reitems
+//
+//  Created by Dennis Anthony on 25/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
