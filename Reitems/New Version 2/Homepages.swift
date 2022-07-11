02fdc3c330c55010f5e130d1fc6db60266f1ed8f
dennisanthonyblabla <dennisanthonyblabla@gmail.com>
2022-07-11 21:55:36 +0700

    Pembuatan ulang homepage

--- Reitems/New Version 2/Homepages.swift
+++ Reitems/New Version 2/Homepages.swift
@@ -47,5 +47,54 @@ extension Homepages: UITableViewDelegate, UITableViewDataSource {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        // Create swipe action
+        let locationDelete = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
+            self.FirstItems.remove(at: indexPath.row)
+            
+            tableView.reloadData()
+        }
+        
+        let locationEdit = UIContextualAction(style: .normal, title: "Edit") { (action, view, completionHandler) in
+            // Which person to remove
+            let locationToEdit = self.FirstItems[indexPath.row]
+            // Edit alert
+            let alert = UIAlertController(title: "Edit Location", message: "Rename the location", preferredStyle: .alert)
+            alert.addTextField()
+            
+            let textField = alert.textFields![0]
+            textField.text = locationToEdit.Item
+            
+            // Configure button handler
+            let saveButton = UIAlertAction(title: "Save", style: .default) { (action) in
+                // Get the textfield for the alert
+                let locationInput = alert.textFields![0]
+                // Edit name property of person object
+                self.FirstItems[indexPath.row].Item = locationInput.text ?? "Fail"
+                tableView.reloadData()
+            }
+            alert.addAction(saveButton)
+            self.present(alert, animated: true)
+        }
     
+        return UISwipeActionsConfiguration(actions: [locationDelete, locationEdit])
+    }
+}
+
+extension Homepages: UITableViewDragDelegate {
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let dragItem = UIDragItem(itemProvider: NSItemProvider())
+        dragItem.localObject = FirstItems[indexPath.row]
+        return [dragItem]
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let mv = FirstItems[sourceIndexPath.row]
+        FirstItems.remove(at: sourceIndexPath.row)
+        FirstItems.insert(mv, at: destinationIndexPath.row)
+        
+        
+        
+    }
 }
